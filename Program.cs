dd37511dc68450c97e95dee776040f216e8a358d
Yaxshiliqov Javlon <97624379+MassiveTitans@users.noreply.github.com>
2024-02-26 14:07:15 +0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -10,9 +10,9 @@ namespace program
             Book kitob_ =new Book(12_000 ,  "Pro_book" , "Mike Tyson");
 
             // Object orqali propertylar yordamida fieldlarning qiymatini o'zgartirish
-            kitob_.Name = "Yangi Nom";
-            kitob_.Author = "Yangi Muallif";
-            kitob_.Price = 75.0;
+            kitob_.Kitob_nomi = "Yangi Nom";
+            kitob_.Kitob_muallifi = "Yangi Muallif";
+            kitob_.Kitob_narxi = 75.0;
             
 
             // get data method
