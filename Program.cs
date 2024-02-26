cee40d7ca6f4afd5ebda724b0e7e9efa9e87dc75
Yaxshiliqov Javlon <97624379+MassiveTitans@users.noreply.github.com>
2024-02-26 14:06:07 +0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using basics; 
+﻿using book; 
 
 
 namespace program
@@ -8,11 +8,21 @@ namespace program
         static void Main()
         {
             Book kitob_ =new Book(12_000 ,  "Pro_book" , "Mike Tyson");
+
+            // Object orqali propertylar yordamida fieldlarning qiymatini o'zgartirish
+            kitob_.Name = "Yangi Nom";
+            kitob_.Author = "Yangi Muallif";
+            kitob_.Price = 75.0;
             
 
             // get data method
 
             kitob_.show_all_data();
+
+            // Skidkani qo'llash
+            myBook.ApplyDiscount(10.0);
+
+            Console.ReadLine();
 
 
         }
