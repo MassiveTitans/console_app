88c1ee2d427353df088660e3367e7e4aaa8eb48a
Yaxshiliqov Javlon <97624379+MassiveTitans@users.noreply.github.com>
2024-02-26 14:08:34 +0500

    Update book.cs

--- book.cs
+++ book.cs
@@ -52,8 +52,19 @@ namespace book
                 Console.WriteLine($"Kitob muallifi: {Kitob_muallifi} \nKitob nomi: {Kitob_nomi} \nKitob narxi: {Kitob_narxi}");
             }
 
+            public void ApplyDiscount(double discountPercentage)
+            {
+                if (discountPercentage < 0 || discountPercentage > 100)
+                {
+                    Console.WriteLine("Xato: Skidkani to'g'ri kiriting!");
+                    return;
+                }
+
+            double discount = price * (discountPercentage / 100);
+            price -= discount;
+
+            Console.WriteLine($"Skidka qo'llandi! Yangi narx: {price}");
+        }
+
     }
-
-
-
 }
